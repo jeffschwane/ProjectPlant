bbab277e0754eed700f47f2a3c68eceb8dff60e4
Jeff Schwane <jschwane@gmail.com>
2021-01-18 18:10:30 -0500

    Added common name vs. latin name and checks for new data

--- plant_data.py
+++ plant_data.py
@@ -1,10 +1,35 @@
 import csv
 import codecs
+import re
 import sqlalchemy
 import datetime as dt
 import pymysql
 # import numpy as np
 # import pandas as pd
+
+
+def insert_data_sql(dates_array, data, plant_id):
+    # iterate through each day
+    for day_index, day in enumerate(dates_array):
+        # iterate through each hour
+        for row in data[plant_id*24: (plant_id+1)*24]:
+            plant_datetime = dt.datetime.strptime(
+                f'{day} {row[0]}', "%Y-%m-%d %H:%M")
+            query = sqlalchemy.insert(readings_table).values(
+                plant_id=plant_id+1,
+                datetime=plant_datetime,
+                light=row[(day_index * num_columns) + 1],
+                soil_moist=row[(day_index * num_columns) + 2],
+                temp=row[(day_index*num_columns) + 3],
+                soil_fert=row[(day_index * num_columns) + 4]
+            )
+            result_proxy = connection.execute(query)
+    # update new last import date
+    new_last_import_date = last_date_data[plant_name].strftime(
+        "%Y-%m-%d")
+    query = sqlalchemy.update(plant_table).values(
+        last_import_date=new_last_import_date).where(plant_table.columns.id == plant_id + 1)
+    result = connection.execute(query)
 
 
 # class Plant():
@@ -60,13 +85,16 @@ with codecs.open('2021-01-07-18-HHCC.csv', 'rU', 'utf-16') as f:
                 start_row = row_index + 3  # the following 3rd line contains the start of the data
         except IndexError:  # necessary since some rows contain no data in columns
             pass
-        if row_index == title_row:  # store plant names and dates
-            # TODO - Pull out common name vs. latin name - use regular expression to find latin name in parenthesis
+        if row_index == title_row:  # store plant names, dates, and last date in the data
             plant_name = row[0]
             plant_dates[plant_name] = ' '.join(row[1::4]).split()
+            # Remove last date so that last date has all hours
+            plant_dates[plant_name].pop()
             last_date_data[plant_name] = plant_dates[plant_name][-1]
+            last_date_data[plant_name] = dt.datetime.strptime(
+                last_date_data[plant_name], "%Y-%m-%d").date()  # Convert to date object
         elif row_index >= start_row:
-            # Clean by removing "--" elements
+            # Clean by replacing "--" elements with zeros
             row[:] = [0 if x == '--' else x for x in row]
             data.append(row)
 
@@ -128,38 +156,40 @@ query = sqlalchemy.select(
     [plant_table.columns.name_common, plant_table.columns.last_import_date])
 result_proxy = connection.execute(query)
 result_set = result_proxy.fetchall()
-print(result_set)
-# TODO - Store result as last_import_date[name_common]
+
+# Store last import dates as dictionary, store plant names
+last_import_date = {}
+sql_plant_list = []
+for plant in result_set:
+    last_import_date[plant[0]] = plant[1]
+    sql_plant_list.append(plant[0])
 
 # Insert plant data into database tables
 num_columns = 4  # four columns of sensor data
 plant_id = 0
 for plant_name, dates_array in plant_dates.items():
-    # Check against date of last import and only update for dates not in database
-    last_datetime_data = dt.datetime.strptime(
-        last_date_data[plant_name], "%Y-%m-%d")
-    # if plant_name in sql table does not exist:
-    # if True:
-    #     query = sqlalchemy.insert(plant_table).values(
-    #         id=plant_id + 1,
-    #         name_common=plant_name,
-    #         last_import_date=last_datetime_data)
-    #     result_proxy = connection.execute(query)
+    common_name = plant_name[0:plant_name.find('(') - 1]
+    latin_name = re.search(r'\((.*?)\)', plant_name).group(1)
 
-    # if last_datetime_data > last_import_date[plant_name]:
-    # iterate through each day
-    # for day_index, day in enumerate(dates_array):
-    #     # iterate through each hour
-    #     for row in data[plant_id*24: (plant_id+1)*24]:
-    #         plant_datetime = dt.datetime.strptime(
-    #             f'{day} {row[0]}', "%Y-%m-%d %H:%M")
-    #         query = sqlalchemy.insert(readings_table).values(
-    #             plant_id=plant_id+1,
-    #             datetime=plant_datetime,
-    #             light=row[(day_index * num_columns) + 1],
-    #             soil_moist=row[(day_index * num_columns) + 2],
-    #             temp=row[(day_index*num_columns) + 3],
-    #             soil_fert=row[(day_index * num_columns) + 4]
-    #         )
-    #         result_proxy = connection.execute(query)
+    # Check if plant name in SQL table does not exist, if so, create entry in plant table
+    if common_name not in sql_plant_list:
+        query = sqlalchemy.insert(plant_table).values(
+            id=plant_id + 1,
+            name_common=common_name,
+            name_latin=latin_name)
+        result_proxy = connection.execute(query)
+
+    # Check against date of last import and only update for dates not in database
+
+    if last_import_date[common_name] is None:
+        insert_data_sql(dates_array, data, plant_id)
+        print(
+            f'Data was successfully imported for {plant_name} through {last_date_data[plant_name].strftime("%Y-%m-%d")}')
+    elif last_import_date[common_name] is not None:
+        if last_date_data[plant_name] > last_import_date[common_name]:
+            insert_data_sql(dates_array, data, plant_id)
+            print(
+                f'More data was imported for {plant_name} through {last_date_data[plant_name].strftime("%Y-%m-%d")}')
+        else:
+            print(f'Data for {plant_name} is up to date.')
     plant_id += 1
